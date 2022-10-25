59b5a4b15b55677f4ec5c4718f17f6fa901bba33
lomo007 <eragon66997@gmail.com>
2022-10-25 12:54:34 +0800

    feat: simple filter user login

--- app.js
+++ app.js
@@ -12,14 +12,16 @@ const exphbs = require('express-handlebars')
 // 引入 mongoose
 require('./config/mongoose')
 
-// 任何請求 都優先使用這個路由 ***
-app.use(routes)
-
 // 指定handlebars樣板引擎,  exphbs 裡面的預設樣板 為 main
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 // view 引擎啟用 為 handlebars
 app.set('view engine', 'handlebars')
 
+// 任何請求 都優先使用 express 內建的 parse-body ***
+app.use(express.urlencoded({ extended: true }))
+
+// 任何請求 都優先使用這個路由 ***
+app.use(routes)
 
 // 路由監聽
 app.listen(port, () => {
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,10 +2,35 @@
 const express = require('express')
 const router = express.Router()
 
+// 引入 Login資料綱要
+const Login = require('../../models/login')
+
 
 // 首頁路由
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+router.post('/', (req, res) => {
+
+  // 接取表單資料
+  console.log(req.body)
+
+  // 判斷資料庫 userEmail 不存在 > 回首頁
+  // ******************* 存在> 判斷資料庫 userPassword 不存在 > 回首頁
+  // ******************* 存在> 導向 welcome 頁面
+
+  Login.findOne({ email: req.body.userEmail, password: req.body.userPassword }, function (err, userEnterFile) {
+
+    if (userEnterFile === null) {
+      const WaningText = "Ooops! Email or Password is wrong, please check and re-try again!"
+      return res.render('index', { WaningText: WaningText })
+    } else if (userEnterFile !== null) {
+      const userName = userEnterFile.firstName
+      return res.render('welcome', { userName: userName })
+    }
+
+  })
 })
 
 
